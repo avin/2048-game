33e1b908ddaeb5f5ff751b6b1883ab724e3f109e
a.vinogradov <a.vinogradov@bpcbt.com>
2021-05-22 18:58:20 +0300

    github link

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,17 +2,25 @@ import React from 'react';
 import { Store } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import Game from '@/components/Game/Game';
+import styles from './styles.module.scss';
+import GitHubLink from '../common/GitHubLink/GitHubLink';
 
 export interface RootProps {
 	store: Store;
 }
 
 const App = ({ store }: RootProps): JSX.Element => (
-	<div id="outer">
-		<Provider store={store}>
+	<Provider store={store}>
+		<div id="outer" className={styles.app}>
+			<div className={styles.headSpace} />
+
 			<Game />
-		</Provider>
-	</div>
+
+			<div className={styles.footer}>
+				<GitHubLink />
+			</div>
+		</div>
+	</Provider>
 );
 
 export default App;
